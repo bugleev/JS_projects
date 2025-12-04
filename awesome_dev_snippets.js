054139ead3a113678f974508ef4574daffe6f0da
Andrey <bugol8@yandex.ru>
2025-12-04 09:59:54 +0100

    Update awesome_dev_snippets.js
    adding typer.js

--- awesome_dev_snippets.js
+++ awesome_dev_snippets.js
@@ -18,3 +18,57 @@ export const sleep = (t = Math.random() * 200 + 300) =>
 export function shuffle(array) {
   return [...array].sort(() => Math.random() - 0.5);
 }
+
+function typer() {
+  let handle;
+  function startBlink() {
+    handle = setInterval(() => {
+      // show/hide .blink
+      const blink = document.querySelector('.blink');
+      if (!blink) return;
+      blink.style.visibility = blink.style.visibility === 'hidden' ? 'visible' : 'hidden';
+    }, 500);
+  }
+  function stopBlink() {
+    const blink = document.querySelector('.blink');
+    blink.style.visibility = 'visible';
+    clearInterval(handle);
+  }
+  
+  const options = ['how', 'are', 'you?'];
+  
+  export async function replaceTyper() {
+    const typer = document.getElementById('typer');
+    if (!typer) return;
+    const current = typer.innerText;
+    const next = options[(options.indexOf(current) + 1) % options.length];
+  
+    const CHAR_DELAY = 40;
+    const WORD_DELAY = 2300;
+  
+    stopBlink();
+  
+    while (typer.innerText.length > 0) {
+      typer.innerText = typer.innerText.slice(0, -1);
+      // wait
+      await new Promise((resolve) => setTimeout(resolve, CHAR_DELAY));
+    }
+  
+    // type in next
+    for (let i = 0; i < next.length; i++) {
+      typer.innerText += next[i];
+      await new Promise((resolve) => setTimeout(resolve, CHAR_DELAY));
+    }
+  
+    startBlink();
+  
+    // wait
+    await new Promise((resolve) => setTimeout(resolve, WORD_DELAY));
+    replaceTyper();
+  }
+  
+  startBlink();
+  setTimeout(() => {
+    replaceTyper();
+  }, 4000);
+}
